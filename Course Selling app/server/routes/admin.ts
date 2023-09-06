c4de488585ce316d5889729da6c02e35f37e604b
DevMadhav13 <madhavkulkarni1305@gmail.com>
2023-09-06 20:00:36 +0530

    ZOD added for Input validation

--- Course Selling app/server/routes/admin.ts
+++ Course Selling app/server/routes/admin.ts
@@ -5,33 +5,42 @@ const router= express.Router();
 import { authenticateJwtAdmin } from "../autentication/admin_auth";
 const secrate1 ="SperS3cRat3"
 
+import { z } from 'zod'; 
+
+let inputProps = z.object({
+  username : z.string().min(1).max(30),
+  password : z.string().min(3).max(30)
+})
+
+let usernameValidation = z.string().min(1).max(30)  
+let PasswordValidation = z.string().min(1).max(30)  
+
+let CourseValidation = z.object({
+  title: z.string().min(1).max(30),
+  description: z.string().min(1).max(300),
+  imageLink : z.string().min(1),
+  price : z.number().min(1)
+})
+
 
 // Admin routes
 router.get('/me',authenticateJwtAdmin,async(req, res) => {
-  // console.log("/me log hitted by madhav")
   const ReqAdmin = req.headers["adminUsername"]
-  // console.log(ReqAdmin)
   const admin = await Admin.findOne({username: ReqAdmin})
   if (admin){
   res.status(200).json(ReqAdmin)
-  // console.log("ReqAdmin sent from in me route is  ")
-  // console.log(ReqAdmin)
-  // console.log(admin)
-
-
-} else {
+  } else {
   res.status(404).json({ message: "Admin not found" });
 }
 
 });
 
 router.post('/signup', async (req, res) => {
-    const newadmin = req.body;
-    const username = req.body.username;
-    console.log("from /signup route")
-    console.log(username)
-    console.log({username})
-
+    const newadmin = inputProps.safeParse(req.body);
+    if (!newadmin.success){
+      return res.status(411).json(newadmin.error)
+    }
+    const username = newadmin.data.username;
     const existingAdmin = await Admin.findOne({username})
     console.log(existingAdmin)
     console.log("from /signup route after admin.find")
@@ -47,24 +56,47 @@ router.post('/signup', async (req, res) => {
   });
   
   router.post('/login',async (req, res) => {
-    const {username,passowrd}=req.headers;
-    const existingAdmin = await Admin.findOne({username,passowrd});
+    const usernamev =usernameValidation.safeParse(req.headers.username);
+    const passowrdv =PasswordValidation.safeParse(req.headers.password);
+    console.log(usernamev)
+    console.log(passowrdv)
+    if (!usernamev.success && !passowrdv.success){
+      return res.status(411).json(usernamev.error|| passowrdv.error)
+    }
+    if (usernamev.success&& passowrdv.success){
+      const username = usernamev.data;
+      const passowrd = passowrdv.data
+      const existingAdmin = await Admin.findOne({username, passowrd});
+      console.log(existingAdmin)
+    
     if(existingAdmin){
+      const username = existingAdmin.username
+      console.log(username)
       const tokan = jwt.sign({username , role: "admin"},secrate1,{expiresIn : '1h'});
       res.status(200).json({message: "Admin login succesful", tokan:tokan})
     }else{
       res.status(200).json({message: "Admin doesnot exist"})
     }
-    
+    }
+
     // logic to log in admin
   });
   
   
   router.post('/addcourse', authenticateJwtAdmin, async(req, res) => {
+    const parsedRes = CourseValidation.safeParse(req.body)
+    if(!parsedRes.success){
+      res.status(300).json(parsedRes.error)      
+    }
+    
+  try {
     const course = new Course(req.body);
     await course.save();
-    res.status(200).json({message:" Course create dsuccesfully"})
-  
+    res.status(200).json({ message: "Course created successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
     // logic to create a course
   });
 
